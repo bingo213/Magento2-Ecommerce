2eb93bd892d66385c5b5060eff0809749cd90bc2
bingo213 <cassiopeiahien@gmail.com>
2021-05-04 09:13:13 +0700

    create new page, hello world step

---
+++ app/code/News/Weather/Controller/Index/Index.php
@@ -0,0 +1,18 @@
+<?php
+namespace News\Weather\Controller\Index;
+class Index extends \Magento\Framework\App\Action\Action
+{
+                protected $_pageFactory;
+                public function __construct(
+                                \Magento\Framework\App\Action\Context $context,
+                                \Magento\Framework\View\Result\PageFactory $pageFactory)
+                {
+                                $this->_pageFactory = $pageFactory;
+                                return parent::__construct($context);
+                }
+                public function execute()
+                {
+                                echo "Hello World";
+                                return $this->_pageFactory->create();
+                }
+}
